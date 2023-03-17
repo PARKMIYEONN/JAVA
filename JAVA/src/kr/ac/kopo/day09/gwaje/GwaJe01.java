5f903aa5fc9ef3fa1a053d150b09b4ba8c741aa1
PARK MIYEON <miyoun1003@naver.com>
2023-03-18 01:45:52 +0900

    과제와 한글땜에 오류난 애들 고쳐서 올림

--- JAVA/src/kr/ac/kopo/day09/gwaje/GwaJe01.java
+++ JAVA/src/kr/ac/kopo/day09/gwaje/GwaJe01.java
@@ -9,38 +9,35 @@ public class GwaJe01 {
 
 		Random r = new Random();
 
-		int random = r.nextInt(100) + 1;
+		int random = r.nextInt(100) + 1; //랜덤 수 설정. 필요한 범위가 1~100이므로 +1
 
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("컴퓨터가 생각하는 1-100사이의 정수를 맞혀보세요");
 		int min = 1;
-		int max = 100;
+		int max = 100; //최솟값 최댓값 초기화
 
-		System.out.printf("%d-%d사이의 정수 입력 : ", min, max);
 
 		for (int i = 0; i <= 5; i++) {
+			System.out.printf("%d-%d사이의 정수 입력 : ", min, max);
 
 			int num = sc.nextInt();
 			System.out.println();
-			if (num < 1 || num > 100) { // 범위 밖의 수를 입력했을 때 다시 입력하게 됨
+			if (num < min || num > max) { // 범위 밖의 수를 입력했을 때 다시 입력하게 됨
 				i--;
-				System.out.printf("%d-%d사이의 정수 입력 : ", min, max);
-			} else if (1 <= num && num < random) {
-				System.out.printf("%d - %d : ", min, max);
-				min = num+1;
-				System.out.printf("%d\n%d보다 큰 수 입니다.\n", num, num);
+			} else if (1 <= num && num < random) { //정답보다 작은 수 일 때
+				min = num+1; //입력값이 정답보다 작으니까 최솟값 다시 설정
+				System.out.printf("%d보다 큰 수 입니다.\n", num, num);
 				System.out.printf("기회는 총 %d번 남았습니다\n\n", 5 - i);
-			} else if (num > random && num <= 100) {
-				System.out.printf("%d - %d : ", min, max);
-				max = num-1;
-				System.out.printf("%d\n%d보다 작은 수 입니다.\n", num, num);
+			} else if (num > random && num <= 100) { //정답보다 큰 수 일 때
+				max = num-1; //입력값이 정답보다 크니까 최댓값 다시 설정
+				System.out.printf("%d보다 작은 수 입니다.\n", num, num);
 				System.out.printf("기회는 총 %d번 남았습니다\n\n", 5 - i);
-			} else {
-				System.out.print("축하합니다. 정답입니다.\n");
+			} else { //정답
+				System.out.print("축하합니다! 정답입니다^^\n");
 				break;
 			}
-			if (5 - i == 0) {
+			if (5 - i == 0) { //기회 소진
 				System.out.printf("아쉽습니다. 기회를 소진하셨습니다.\n정답은 %d입니다", random);
 				break;
 			}
